44a132aac12c1906b67cf94843bc26a4427e2168
YukaUeno <156647522+NapSnail@users.noreply.github.com>
2024-01-31 00:05:13 +0900

    課題提出

---
+++ src/bases/Monster.java
@@ -0,0 +1,29 @@
+package bases;
+
+import utils.Dice;
+
+//モンスターを表現する抽象クラス
+public abstract class Monster extends Living {
+
+	// コンストラクタ
+	public Monster(String name, String weapon) {
+		// Livingクラスで定義したコンストラクタを利用する
+		super(name, weapon);
+	}
+
+	// attackメソッドのオーバーライド
+	@Override
+	public void attack(Living target) {
+		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
+		int damage = (Dice.get(1, 10) * getOffensive());
+
+		// 相手のHPをダメージ値だけ減らす
+		int hp = target.getHp() - damage;
+		target.setHp(hp);
+
+		// コンソールにステータスを表示
+		System.out.println(
+				" 「" + this.name + "」が「" + this.weapon + "」で攻撃！「" + target.getName() + "」に「" + damage + "」のダメージを与えた。");
+
+	}
+}
